81117b74e44c3a3ed1122c893fea48a6e1eb8977
Ziyu <lia.ziyu@gmail.com>
2022-03-08 15:42:03 +0100

    question 4.1

--- pfx/basic/ast.ml
+++ pfx/basic/ast.ml
@@ -1,14 +1,28 @@
 type command =
-  DefineMe (* Question 4.1 *)
+|Push 
+|Pop
+|Swap
+|Add 
+|Div
+|Mul
+|Rem
+|Sub;;
 
 type program = int * command list
 
 (* add here all useful functions and types  related to the AST: for instance  string_of_ functions *)
 
 let string_of_command = function
-  | DefineMe -> "to be done"
+|Push -> "push"
+|Pop -> "pop"
+|Swap -> "swap"
+|Add -> "add"
+|Div -> "div"
+|Mul -> "mul"
+|Rem -> "rem"
+|Sub -> "sub";;
 
-let string_of_commands cmds = String.concat " " (List.map string_of_command cmds)
+let string_of_commands cmds = String.concat " " (List.map string_of_command cmds);;
 
-let string_of_program (args, cmds) = Printf.sprintf "%i args: %s\n" args (string_of_commands cmds)
+let string_of_program (args, cmds) = Printf.sprintf "%i args: %s\n" args (string_of_commands cmds);;
 
